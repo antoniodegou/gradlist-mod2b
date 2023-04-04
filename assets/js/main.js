325ede4fd9e3ca01f9133a45328081fbf1d5b1c6
antonio de gou <thestockofantonio@gmail.com>
2023-04-04 16:35:44 +0100

    add dependencies bstrap, sass, and color APIs; and configured vite

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import '/assets/css/style.scss'
+
+ 
+
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default {
+    base: "./",
+    build: {
+      outDir : "docs",
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    }
+
+  }
