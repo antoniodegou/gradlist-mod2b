cb173e65331e17303abac7589c2c02152fea61e6
antonio de gou <thestockofantonio@gmail.com>
2023-04-05 09:57:16 +0100

    removed globals from addremove.js, left spotColList as global

--- assets/js/addRemove.js
+++ assets/js/addRemove.js
@@ -3,15 +3,13 @@ import outputList from '/assets/js/listOptions'
 import addSpotColourDOM from '/assets/js/addSpotColourDOM'
 
 let spotColList = []
-const submit = document.getElementById("submit")
-const spotCols = document.getElementById("spotcols")
-
-let removeButtons = document.querySelectorAll(".remove")
+ 
 
 /*
 // add event listener for "add colour" bottom
 */
-submit.addEventListener("click", function (e) {
+document.getElementById("submit").addEventListener("click", function (e) {
+    const spotCols = document.getElementById("spotcols")
     e.preventDefault()
     let hex = document.getElementById("hex_code")
     spotColList.push(hex.value) //add to array
@@ -19,33 +17,16 @@ submit.addEventListener("click", function (e) {
     colPreview(spotColList) //add to preview
     outputList(spotColList)  //output gradient list
 
+    // make sure all event listeners are removed
     document.querySelectorAll(".remove").forEach(function (elem) {
         elem.removeEventListener('click',removeButtonListener)
     })
 
-    removeButtons = document.querySelectorAll(".remove");
-
-    removeButtons.forEach(function (elem) {
+    // rebuild event listeners for the new remove buttons
+    document.querySelectorAll(".remove").forEach(function (elem) {
         elem.addEventListener("click", removeButtonListener)
     })
 
-    // removeButtons.forEach(function (elem) {
-    //   elem.addEventListener("click", function (e) {
-    //       e.preventDefault();
-    //       // remove from col array
-    //       let getElementDelete = e.target.parentNode.getAttribute("data-col")
-    //       spotColList = spotColList.filter((e, i) => i.toString() != getElementDelete)
-    //       //remove from dom
-    //       elem.parentNode.remove()
-    //       //reorder dom index number
-    //       let updatedata = document.querySelectorAll("[data-col]")
-    //       updatedata.forEach((el, i) => (el.dataset.col = i.toString()))
-    //       // update preview
-    //       colPreview()
-    //       outputList()
-    //       // use once:true to remove event listener from deleted colour
-    //   },{ once: true });
-    // });
 });
 
 const removeButtonListener = (e ) => {
@@ -64,30 +45,6 @@ const removeButtonListener = (e ) => {
     // use once:true to remove event listener from deleted colour
 }
 
-/*
-// Add and refresh event listeners for new "remove" bottons
-*/
-// submit.addEventListener("click", function (e) {
-//   removeButtons = document.querySelectorAll(".remove");
-
-//   removeButtons.forEach(function (elem) {
-//     elem.addEventListener("click", function (e) {
-//         e.preventDefault();
-//         // remove from col array
-//         let getElementDelete = e.target.parentNode.getAttribute("data-col")
-//         spotColList = spotColList.filter((e, i) => i.toString() != getElementDelete)
-//         //remove from dom
-//         elem.parentNode.remove()
-//         //reorder dom index number
-//         let updatedata = document.querySelectorAll("[data-col]")
-//         updatedata.forEach((el, i) => (el.dataset.col = i.toString()))
-//         // update preview
-//         colPreview()
-//         outputList()
-//         // use once:true to remove event listener from deleted colour
-//     },{ once: true });
-//   });
-// });
 
 
 export default spotColList
