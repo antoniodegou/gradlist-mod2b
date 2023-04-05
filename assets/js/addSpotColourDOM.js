3686525d4094a067130ca5842d3ebb23d94e3f7f
antonio de gou <thestockofantonio@gmail.com>
2023-04-05 22:16:29 +0100

    removed placeholder from inputs, added title to spot colour remove buttons

--- assets/js/addSpotColourDOM.js
+++ assets/js/addSpotColourDOM.js
@@ -1,14 +1,16 @@
 /*
 // Add new Spot colours to the DOM
 */
+import * as bootstrap from 'bootstrap'
 const addSpotColourDOM = (spotColList) => {
     let a = "";
     spotColList.forEach((item, i) => {
       a += `<div class="row spot-col mb-4  " data-col="${i}" >
-              <div class="col-2 remove  d-flex align-items-center justify-content-center" data-icon="&#xe905;"></div>
+              <div class="col-2 remove  d-flex align-items-center justify-content-center" data-icon="&#xe905;"   title="Remove Colour" data-bs-placement="bottom"></div>
               <div class="col-10  colour-demo" style="background-color:#${item}"></div>
           </div>`
     });
+
     return a;
   };
   
